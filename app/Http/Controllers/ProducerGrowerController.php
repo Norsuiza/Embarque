c3de9a3e36524b8e1f860b101c1b34b73837c74f
Diego Osuna <diegosuna2005@yahoo.com>
2024-07-20 19:12:48 -0700

    Creacion de modales, migraciones y controladores

---
+++ app/Http/Controllers/ProducerGrowerController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producer_Grower;
+use Illuminate\Http\Request;
+
+class ProducerGrowerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Producer_Grower $producer_Grower)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Producer_Grower $producer_Grower)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Producer_Grower $producer_Grower)
+    {
+        //
+    }
+}
